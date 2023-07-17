60662baa2056ac304875b3c972a2cfd91d8492af
Bruce0203 <BruceFreedy@gmail.com>
2023-07-17 19:36:54 +0900

    init

--- client/src/commonMain/kotlin/ComponentHookController.kt
+++
@@ -1,6 +0,0 @@
-import com.github.quillraven.fleks.ComponentHook
-
-interface ComponentHookController<T> {
-    val onAdded: ComponentHook<T>
-    val onRemoved: ComponentHook<T>
-}
--- client/src/commonMain/kotlin/World.kt
+++ client/src/commonMain/kotlin/World.kt
@@ -3,10 +3,10 @@ import com.github.quillraven.fleks.world
 import components.Sprite
 import korlibs.korge.scene.SceneContainer
 import korlibs.korge.view.addUpdater
-import org.koin.core.definition.Callbacks
 
 fun world(sceneContainer: SceneContainer, init: WorldConfiguration.() -> Unit = {}) = world {
     components {
+
         onAdd(Sprite, Sprite.onComponentAdded)
         onRemove(Sprite, Sprite.onComponentRemoved)
     }
