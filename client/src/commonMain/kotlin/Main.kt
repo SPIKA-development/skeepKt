3280f814cfddac0e85db1944e9ce73f121e21686
Bruce0203 <BruceFreedy@gmail.com>
2023-07-17 20:40:09 +0900

    init

--- client/src/commonMain/kotlin/Main.kt
+++ client/src/commonMain/kotlin/Main.kt
@@ -1,7 +1,8 @@
+import korlibs.image.color.Colors
 import korlibs.korge.Korge
 import korlibs.korge.scene.sceneContainer
 import scene.Game
 
-suspend fun main() = Korge {
+suspend fun main() = Korge(backgroundColor = Colors.BLACK) {
     sceneContainer().changeTo({ Game() })
 }
