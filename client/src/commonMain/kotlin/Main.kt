b8d7081e812f5973c027e647b2f45ef6bf0103ac
Bruce0203 <BruceFreedy@gmail.com>
2023-07-18 09:38:10 +0900

    init

--- client/src/commonMain/kotlin/Main.kt
+++ client/src/commonMain/kotlin/Main.kt
@@ -1,8 +1,9 @@
 import korlibs.image.color.Colors
 import korlibs.korge.Korge
 import korlibs.korge.scene.sceneContainer
+import korlibs.math.geom.ScaleMode
 import scene.Game
 
-suspend fun main() = Korge(backgroundColor = Colors.PAPAYAWHIP) {
+suspend fun main() = Korge(scaleMode = ScaleMode.COVER, backgroundColor = Colors.PAPAYAWHIP) {
     sceneContainer().changeTo({ Game() })
 }
--- client/src/commonMain/kotlin/scene/Game.kt
+++ client/src/commonMain/kotlin/scene/Game.kt
@@ -17,7 +17,7 @@ class Game : Scene() {
     override suspend fun SContainer.sceneInit() {
         SpriteAssets.load()
         println("A")
-        scaleAvg = 4.5f
+        scaleAvg = 4f
         injector.apply {
             mapSingleton { sceneContainer }
             mapSingleton {
