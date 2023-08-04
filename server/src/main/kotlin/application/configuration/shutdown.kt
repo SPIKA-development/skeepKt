cfcd66cedae1b7598afbc2aa63494f3ba55b078f
Bruce0203 <BruceFreedy@gmail.com>
2023-08-04 23:12:25 +0900

    init

--- server/src/main/kotlin/application/configuration/shutdown.kt
+++ server/src/main/kotlin/application/configuration/shutdown.kt
@@ -1,18 +1,49 @@
 package application.configuration
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.engine.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.launch
+import kotlin.system.exitProcess
 
 fun Application.configurationShutdown() {
-    val shutdown = ShutDownUrl("") { 0 }
     routing {
         post("shutdown") {
             val key = EnvVar.ADMIN_KEY
             if (call.parameters["key"] == key) {
-                shutdown.doShutdown(call)
+                doShutdown(call)
             }
         }
+    }
+}
+
+public suspend fun doShutdown(call: ApplicationCall) {
+    call.application.log.warn("Shutdown URL was called: server is going down")
+    val application = call.application
+    val environment = application.environment
+    val exitCode = 0
+
+    val latch = CompletableDeferred<Nothing>()
+    call.application.launch {
+        latch.join()
+
+        environment.monitor.raise(ApplicationStopPreparing, environment)
+        if (environment is ApplicationEngineEnvironment) {
+            environment.stop()
+        } else {
+            application.dispose()
+        }
+
+        exitProcess(exitCode)
+    }
+
+    try {
+        call.respond(HttpStatusCode.Gone)
+    } finally {
+        latch.cancel()
     }
 }
